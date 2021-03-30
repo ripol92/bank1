73a55b580898423a30fa09df1b7bf8d64d01f93b
Furkat Subkhonkulov <subhonkulov2@gmail.com>
2021-03-30 18:14:51 +0500

    added example

--- pkg/bank/stats/example_test.go
+++ pkg/bank/stats/example_test.go
@@ -1,3 +1,19 @@
 package stats
 
+import (
+	"github.com/ripol92/bank/pkg/bank/types"
+	"fmt"
+)
 
+func ExampleAvg() {
+	payments := []types.Payment{
+		{ID: 1, Amount: types.Money(10)},
+		{ID: 2, Amount: types.Money(20)},
+		{ID: 3, Amount: types.Money(30)},
+	}
+
+	avg := Avg(payments)
+	fmt.Println(avg)
+
+	// Output: 20
+}
