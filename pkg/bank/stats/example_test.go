a91398024e548d47af3b25e70bf5a7e9013611f6
Furkat Subkhonkulov <subhonkulov2@gmail.com>
2021-03-30 18:33:28 +0500

    added totalInCategory function

--- pkg/bank/stats/example_test.go
+++ pkg/bank/stats/example_test.go
@@ -17,3 +17,18 @@ func ExampleAvg() {
 
 	// Output: 20
 }
+
+func ExampleTotalInCategory() {
+	category := types.Category("card")
+
+	payments := []types.Payment {
+		{ID: 1, Amount: types.Money(10), Category: types.Category("overdraft")},
+		{ID: 2, Amount: types.Money(20), Category: types.Category("card")},
+		{ID: 3, Amount: types.Money(30), Category: types.Category("card")},
+	}
+
+	total := TotalInCategory(payments, category)
+	fmt.Println(total)
+
+	// Output: 50
+}
--- pkg/bank/stats/stats.go
+++ pkg/bank/stats/stats.go
@@ -13,3 +13,14 @@ func Avg(payments []types.Payment) types.Money {
 
 	return sum / types.Money(count)
 }
+
+func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
+	sum := types.Money(0)
+	for _, payment := range payments {
+		if payment.Category == category {
+			sum += payment.Amount
+		}
+	}
+
+	return sum
+}
